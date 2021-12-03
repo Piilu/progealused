fee1aae3172ec4d145a011c4b00dd3c86d27b721
Piilu <piilrainer@gmail.com>
2021-12-03 17:05:08 +0200

    commit

--- checkTemp.js
+++
@@ -1,7 +0,0 @@
-//0-20 liiga külm   -1
-//21-40 paras        0
-//41-60 liiga külm   1
-
-function checkTemp(temp){
-    asd
-}
---
+++ kodutööd/checkTemp.js
@@ -0,0 +1,19 @@
+//0-20 liiga külm   -1
+//21-40 paras        0
+//41-60 liiga kuum   1
+
+function checkTemp(temp){
+   if(temp <=20){
+       console.log("liiga külm");
+       return -1;
+   }
+   else if (temp>=41) {
+       console.log("liiga kuum");
+       return 1;
+   }
+   else{
+       console.log("paras");
+       return 0;
+   }
+}
+checkTemp(80);
